7a0f52083c6dcaf89508cf9831bdaae48c24557f
sodino <sodino@qq.com>
2016-09-29 22:18:20 +0800

    正文及其链接分段截取及动态加载显示(但可点击事件仍然无效？待解决)

--- js/NewsArticle.js
+++ js/NewsArticle.js
@@ -10,8 +10,10 @@ import {
     WebView,
     Navigator,
     TouchableWithoutFeedback,
+    ScrollView,
 } from 'react-native';
 import api from './api.js';
+import Cheerio from 'cheerio';
 
 export default class NewsArticle extends Component {
     constructor(props) {
@@ -27,16 +29,152 @@ export default class NewsArticle extends Component {
         this._onRefresh();
     }
 
+    _parseHtml(html) {
+        var $ = Cheerio.load(html);
+        var article = {
+            ...this.state.dataArticle,
+            content : '',
+            viewCount : 0,
+            imgs : [],
+            aHrefs : [],
+        };
+
+        var $ = Cheerio.load(html);
+        var $article = $('article');
+        $article.find('span').remove();
+        $article.find('h2').remove();
+        $article.find('br').remove();
+        $article.find('p').each((index, item_p)=>{
+            let p_str = $(item_p).text();
+            if (!p_str) {
+                p_str = '';
+            }
+            if (p_str.startsWith('本文已被查看')) {
+                let viewCount = p_str.replace('本文已被查看','').replace('次','').trim();
+                article.viewCount = viewCount;
+                //console.log('viewCount=%d', viewCount);
+                $(item_p).remove(); // 清空'本文已被查看 n 次'
+            }
+        });
+
+        $article.find('img').each((index, item_img)=>{
+            let $item_img = $(item_img);
+            let src = $item_img.attr('src');
+            article.imgs.push({img : src});
+            //console.log('index=%s src=%s', index, src);
+        });
+
+        article.content = $article.text().replace(/(\r|\n)/g, '').trim();
+        var seekTo = 0;
+        $article.find('a').each((index, item_a)=>{
+            let $item_a = $(item_a);
+            let str = $item_a.text();
+            let href = $item_a.attr('href');
+
+            let preContent = this._cutContent(article.content, seekTo, str);
+
+            seekTo = seekTo + preContent.length + str.length;
+            article.aHrefs.push({txt : preContent});
+            article.aHrefs.push({txt : str, href : href});
+            //console.log('index=%s str=%s href=%s', index, str, href);
+        });
+
+        if (seekTo < article.content.length) {
+            let endContent = article.content.substr(seekTo);
+            console.log('end=[%s]', endContent);
+            article.aHrefs.push({txt : endContent});
+        }
+
+        this.setState({
+            dataArticle:article,
+        });
+        console.log(JSON.stringify(article));
+    }
+
     _onRefresh() {
         this.setState({refreshing : true});
 
-        var url = api.Article + this.state.dataArticle.sid;
+//        var url = api.Article + this.state.dataArticle.sid;
+        var url = api.Article + '49839';
 
+        var headers = new Headers();
+        headers.append('User-Agent', '(Android)');
+        // Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+        headers.append('Accept-Encoding', 'deflate');
+        headers.append('Accept-Language', 'zh-CN,zh;q=0.8');
+        var request = new Request(url, {method :'GET', headers: headers});
+
+        fetch(request)
+            .then((response)=>{
+                return response.text();
+            })
+            .then((html)=>{
+                this._parseHtml(html);
+            })
+            .catch((error)=>{
+                console.log('NewsAritcle::_onRefresh() error=%s', error);
+            }).done(()=>{
+            // 取消转圈
+            this.setState({
+                refreshing : false
+            });
+        });
     }
 
+    _cutContent(content, seekTo, keyEnd) {
+        var idxKey = content.indexOf(keyEnd, seekTo);
+        var preContent = content.substr(seekTo, idxKey - seekTo);
+        // console.log('seekTo=%s idxKey=%s pre=[%s] key=[%s]', seekTo, idxKey, preContent, keyEnd);
+        return preContent;
+    }
 
+    _assembleArticleContent(article) {
+        var arrTxt = [];
+        if (!article.content) {
+            // 还没有获取到内容
+            arrTxt.push(this._newText(article.description));
+        } else {
+            var content = article.content;
+            var aHrefs = article.aHrefs;
+            var seekTo = 0;
+
+            aHrefs.forEach((href, index, aHrefs) => {
+                var txt = href.txt;
+                var href = href.href;
+
+                arrTxt.push(this._newText(txt, href));
+            });
+        }
+
+        return arrTxt;
+    }
+
+    _newText(strText, href) {
+        if (!href) {
+            // 普通文本
+            return (<Text>{strText}</Text>);
+        } else {
+            // 带超链接的文本，需要处理点击事件
+            return (<Text style={{textDecorationLine: 'underline'}}
+                        onPress={()=>{
+                            this._jumpToWeb.bind(this, href)
+                        }}>
+                            {strText}
+                        </Text>);
+        }
+    }
+
+    _jumpToWeb(href) {
+        this.props.navigator.push({
+            id: 'web',
+            url: href,
+            title: 'Referer URL',
+        });
+    }
 
     render() {
+        var arrTxt = this._assembleArticleContent(this.state.dataArticle);
+
         // TouchableWithoutFeedback没有width height backgroundColor等属性，真难用
         // onPress直接赋值为navigator.pop，也可以写个函数执行()=>{pop}
         return (
@@ -49,7 +187,11 @@ export default class NewsArticle extends Component {
                     </TouchableWithoutFeedback>
                     <Image source={{uri : 'title'}} style={{width : 175, height : 35}}></Image>
                 </View>
-
+                <View style={{flex : 1, justifyContent : 'flex-start',}}>
+                    <Text key="articleTextContent">
+                        {arrTxt}
+                    </Text>
+                </View>
             </View>
         );
     }
